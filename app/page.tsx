3e5bacff4c885d1e375c9da9f6d80ca3d760baf8
René Hellenes <rene@deckmatch.com>
2026-06-01 19:19:53 +0200

    Fixing uncached data accessed outside suspense

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Profile } from '@/app/components/pokemon/Profile';
 import { SearchBar } from '@/app/components/pokemon/SearchBar';
 import type { SearchParams } from '@/app/lib/types';
 
-export default async function Home({
+async function SearchBarWithQuery({
   searchParams,
 }: {
   searchParams: SearchParams;
@@ -13,6 +13,14 @@ export default async function Home({
   const { pokemon } = await searchParams;
   const queried = Array.isArray(pokemon) ? pokemon[0] : pokemon;
 
+  return <SearchBar defaultValue={queried} />;
+}
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
   return (
     <ErrorBoundary
       title="Could not load this Pokemon"
@@ -21,7 +29,9 @@ export default async function Home({
     >
       <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex w-full max-w-3xl flex-col items-stretch gap-6 py-16 px-16 bg-white dark:bg-black">
-          <SearchBar defaultValue={queried} />
+          <Suspense fallback={<SearchBar />}>
+            <SearchBarWithQuery searchParams={searchParams} />
+          </Suspense>
           <Suspense>
             <Profile searchParams={searchParams} />
           </Suspense>
